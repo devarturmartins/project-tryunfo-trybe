38c604034a04523bd8627d3734c22ad4d911fc0b
devarturmartins <arturvl1@hotmail.com>
2022-10-27 11:06:34 -0300

    r.3

---
+++ src/components/Card.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+    return (
+      <>
+        <p data-testid="name-card">{ cardName }</p>
+        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+        <p data-testid="description-card">{ cardDescription }</p>
+        <p data-testid="attr1-card">{ cardAttr1 }</p>
+        <p data-testid="attr2-card">{ cardAttr2 }</p>
+        <p data-testid="attr3-card">{ cardAttr3 }</p>
+        <p data-testid="rare-card">{ cardRare }</p>
+        {
+          cardTrunfo ? <p data-testid="trunfo-card">Super Trunfo</p> : <p>{}</p>
+        }
+      </>
+    );
+  }
+}
+export default Card;
