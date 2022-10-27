2c013e19328bb6fe40090e75a7e48785db04c3ae
devarturmartins <arturvl1@hotmail.com>
2022-10-27 18:47:25 -0300

    r. 5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Card from './components/Card';
 import Form from './components/Form';
 
+const maxValue = 90;
+const minValue = 0;
+const somaValues = 210;
+
 class App extends React.Component {
   state = {
     cardName: '',
@@ -12,13 +16,41 @@ class App extends React.Component {
     cardAttr3: '',
     cardRare: 'raro',
     cardTrunfo: false,
+    isDisabled: true,
+  };
+
+  isSaveButtonDisabled = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+    } = this.state;
+    const validation = cardName.length === 0
+      || cardDescription.length === 0
+      || cardImage.length === 0
+      || cardRare === 0
+      || Number(cardAttr1) > maxValue
+      || Number(cardAttr2) > maxValue
+      || Number(cardAttr3) > maxValue
+      || Number(cardAttr1) < minValue
+      || Number(cardAttr2) < minValue
+      || Number(cardAttr3) < minValue
+      || (Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3)) > somaValues;
+    this.setState({
+      isDisabled: validation,
+    });
   };
 
   onInputChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value,
+    }, () => {
+      this.isSaveButtonDisabled();
     });
-    console.log(event.target.value);
   };
 
   render() {
@@ -29,7 +61,8 @@ class App extends React.Component {
       cardAttr2,
       cardAttr3,
       cardRare,
-      cardTrunfo } = this.state;
+      cardTrunfo,
+      isDisabled } = this.state;
     return (
       <>
         <h1>Tryunfo</h1>
@@ -43,6 +76,7 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
           onInputChange={ this.onInputChange }
+          isSaveButtonDisabled={ isDisabled }
         />
         <Card
           cardName={ cardName }
